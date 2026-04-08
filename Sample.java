9a07fd21ae9ea758d3abe29a64baf7df7368d256
dhruvil15 <76967484+dhruvil15@users.noreply.github.com>
2026-04-07 20:21:11 -0400

    complete Desgin-1

--- Sample.java
+++ Sample.java
@@ -1,7 +1,34 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : O(1)
+// Space Complexity : O(1) -> O(1000001)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : Initialized the boolean set with only 100 as the limit
 
 
 // Your code here along with comments explaining your approach
+class MyHashSet {
+    boolean[] set;
+
+    public MyHashSet() {
+        set = new boolean[1000001];
+    }
+
+    public void add(int key) {
+        set[key] = true;
+    }
+
+    public void remove(int key) {
+        set[key] = false;
+    }
+
+    public boolean contains(int key) {
+        return set[key];
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
